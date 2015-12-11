e537ac36696e1299a2d996e32509eb5629024819
Stanislav Venzerul <stanislav_venzerul@silanis.com>
2015-12-10 23:53:50 -0500

    basic structure, getting the /projects end point to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,41 @@
+var gulp = require('gulp');
 var babel = require('gulp-babel');
-var watch = require('gulp-watch');kjj
+var watch = require('gulp-watch');
+var rimraf = require('rimraf');
+var mocha = require('gulp-mocha');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
 
-var buildDir = 'build';
-var srcDir = "src/**/*.js";
+var buildPath = 'dist';
+var srcGlob = 'lib/**/*.js';
+var testGlob = 'tests/rally-manager/*.js';
+var watchGlob = [srcGlob, testGlob];
 
-var paths = {
-  src: srcDir
-}
-
-//basic babel task
-gulp.task('babel', function() {
-  return gulp.src(paths.src)
-  .pipe(babel())
-  .pipe(gulp.dest('./'))
-})
-
-gulp.task('default', ['babel']);
-
-//the watch task
-gulp.task('watch', function() {
-  gulp.watch(paths.babel, ['babel', 'pm2'])
+gulp.task('clean', function(cb) {
+  rimraf('dist/*', cb);
 });
+
+gulp.task('build', ['clean'], function() {
+  return gulp.src(srcGlob)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .on('error', function(e) {
+      console.log(e);
+    })
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(buildPath));
+});
+
+gulp.task('watch', function() {
+  return gulp.watch(watchGlob, ['build', 'test-cli']);
+});
+
+gulp.task('test-cli', ['build'], function() {
+  return gulp.src(testGlob, {read: false})
+    .pipe(mocha());
+});
+
+gulp.task('default', ['watch']);
