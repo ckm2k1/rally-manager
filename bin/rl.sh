20077e1b7164c57b55e621fcaeb8734c6d0e9e9b
Stanislav Venzerul <stanislav_venzerul@silanis.com>
2015-12-25 15:36:14 -0500

    adding refCache, much refactor, such code

--- bin/rl.sh
+++ bin/rl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-node --harmony --harmony_modules --harmony_destructuring --harmony_rest_parameters --harmony_arrow_functions --harmony_spreadcalls --harmony_object --harmony_default_parameters lib/index.js "$@"
+node --harmony --harmony_modules --harmony_destructuring --harmony_rest_parameters --harmony_arrow_functions --harmony_spreadcalls --harmony_object --harmony_default_parameters --harmony_reflect lib/index.js "$@"
 
