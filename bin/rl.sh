ac711cd2643bda2bb33eb386a5829b38d4707b12
Steve Venzerul <stanislav_venzerul@mcafee.com>
2016-01-06 15:57:08 -0500

    using correct variable for collectionName

--- bin/rl.sh
+++ bin/rl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-node --harmony --harmony_modules --harmony_destructuring --harmony_rest_parameters --harmony_arrow_functions --harmony_spreadcalls --harmony_object --harmony_default_parameters --harmony_reflect lib/index.js "$@"
+node --harmony --harmony_modules --harmony_destructuring --harmony_rest_parameters --harmony_arrow_functions --harmony_spreadcalls --harmony_object --harmony_default_parameters --harmony_reflect ~/Programming/rally-manager/lib/index.js "$@"
 
